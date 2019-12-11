8b4b69508830fd3a07e7c1cbfca88a0b37aeffe3
Bob Cowdery <bob@bobcowdery.plus.com>
2019-12-11 19:53:19 +0000

    Developing.

---
+++ src/cpp/JuceConsole/Source/RadioWindows/radio_component.cpp
@@ -0,0 +1,75 @@
+/*
+radio_component.cpp
+
+Content for a radio window.
+
+Copyright (C) 2019 by G3UKB Bob Cowdery
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+The authors can be reached by email at:
+
+	bob@bobcowdery.plus.com
+
+*/
+
+#include "radio_component.h"
+#include "../Common/gui_cache.h"
+
+//==============================================================================
+RadioComponent::RadioComponent(String radio_id)
+{
+	// Register component
+	GUICache::getInstance()->setMainInst(this);
+}
+
+RadioComponent::~RadioComponent()
+{
+}
+
+void RadioComponent::start_ui()
+{
+	vfo_component = new VFOComponent("radio-1", RX);
+	addAndMakeVisible(vfo_component);
+	GUICache::getInstance()->setVFOInst(vfo_component);
+
+	mode_panel = new ModePanel("radio-1");
+	addAndMakeVisible(mode_panel);
+
+	filter_panel = new FilterPanel("radio-1");
+	addAndMakeVisible(filter_panel);
+
+	display_panel = new DisplayPanel("radio-1");
+	addAndMakeVisible(display_panel);
+
+	setSize(600, 450);
+}
+
+//==============================================================================
+void RadioComponent::paint (Graphics& g)
+{
+    // The component is opaque, so we must completely fill the background with a solid colour
+	g.fillAll(Colours::darkgrey);
+}
+
+void RadioComponent::resized()
+{
+    // This is called when the MainComponent is resized.
+    // Resize children
+	vfo_component->setBounds(10, 50, getWidth() - 20, 80);
+	mode_panel->setBounds(10,140, (getWidth()/2) + 20, 100);
+	filter_panel->setBounds((getWidth()/2) + 40, 140, (getWidth()/2) - 50, 100);
+	display_panel->setBounds(10, 245, getWidth() - 20, getHeight() - 255);
+}
---
+++ src/cpp/JuceConsole/Source/RadioWindows/radio_component.h
@@ -0,0 +1,64 @@
+/*
+radio_component.h
+
+Content for a radio window.
+
+Copyright (C) 2019 by G3UKB Bob Cowdery
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+The authors can be reached by email at:
+
+	bob@bobcowdery.plus.com
+
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "../Components/vfo.h"
+#include "../Components/radio_buttons.h"
+#include "../Components/mode_panel.h"
+#include "../Components/filter_panel.h"
+#include "../Components/display_panel.h"
+
+//==============================================================================
+/*
+    Radio component which contains all content for one radio instance
+*/
+class RadioComponent : public Component
+{
+public:
+    //==============================================================================
+	RadioComponent(String radio_id);
+    ~RadioComponent();
+
+	void start_ui();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    //==============================================================================
+    // Children
+	StartButton *start_button;
+	VFOComponent *vfo_component;
+	ModePanel *mode_panel;
+	FilterPanel *filter_panel;
+	DisplayPanel *display_panel;
+	
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RadioComponent)
+};
