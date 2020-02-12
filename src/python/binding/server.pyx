074c6baeca5417def6e2964262e21041d139de40
Bob Cowdery <bob@bobcowdery.plus.com>
2020-02-12 22:25:30 +0000

    Developing.

--- src/python/binding/server.pyx
+++ src/python/binding/server.pyx
@@ -1,19 +1,38 @@
 # server.pyx
 
+"""
+    This module wraps the SDRLibE server lib.
+    it is a straight mapping, there is no logic other than marshalling.
+"""
+
+# Import the API defs
 cimport server_api as api
 
+# ===================================================
+# Server functions
 cdef class Server:
-    
+
+    # ----------------------------
+    # Constructor
     def __cinit__(self):
         pass
 
-    # ===================================================
-    # Initialisation
+    # ----------------------------
+    # Server initialisation
     def cinit(self):
         api.c_server_init()
         
-    # ===================================================
-    # Audio device enumeration
+# ===================================================
+# Audio functions
+cdef class Audio:
+
+    # ----------------------------
+    # Constructor
+    def __cinit__(self):
+        pass
+    
+    # ----------------------------
+    # Device enumerations
     def enum_inputs(self):
         """ Enum compatible input devices """
         
@@ -26,11 +45,22 @@ cdef class Server:
         devs = api.c_server_enum_audio_outputs()
         return __convert_device_list(devs)
 
+# ===================================================
+# Radio functions
+cdef class Radio:
+
+    # ----------------------------
+    # Constructor
+    def __cinit__(self):
+        pass
+    
+
 #=======================================================================================	
 # These are functions with C types. They can be called internally in Cython but cannot be exposed.
+
+# ----------------------------
+# Convert an audio device list into a python array of entries
 cdef __convert_device_list(api.DeviceEnumList* devs):
-    """ Convert from DeviceEnumList to Python dict """
-    
     device_list = []
     for n in range(devs.entries):
         if devs.devices[n].name.decode("ascii").lower().find('sound mapper') == -1:
