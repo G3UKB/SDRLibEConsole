cc89f0ddd3461306f86271e50a182c5442ebf765
Bob Cowdery <bob@bobcowdery.plus.com>
2020-02-13 17:18:18 +0000

    Developing.

--- src/python/binding/server.pyx
+++ src/python/binding/server.pyx
@@ -8,6 +8,10 @@
 # Import the API defs
 cimport server_api as api
 
+# Numpy imports
+import numpy as np
+np.import_array()
+
 # ===================================================
 # Server functions
 cdef class Server:
@@ -20,7 +24,7 @@ cdef class Server:
     # ----------------------------
     # Server initialisation
     def cinit(self):
-        api.c_server_init()
+        return api.c_server_init()
     
     # ----------------------------
     # Start server
@@ -30,7 +34,7 @@ cdef class Server:
     # ----------------------------
     # Terminate server
     def server_terminate(self):
-        api.c_server_terminate()
+        return api.c_server_terminate()
         
 # ===================================================
 # Audio functions
@@ -60,13 +64,13 @@ cdef class Audio:
     
     # ----------------------------
     # Clear all routes    
-    def server_clear_audio_routes():
-        api.c_server_clear_audio_routes()
+    def server_clear_audio_routes(self):
+        return api.c_server_clear_audio_routes()
     
     # ----------------------------
     # Restart audio routes after cleas and set commands    
-    def server_restart_audio_routes():
-        api.c_server_restart_audio_routes()
+    def server_restart_audio_routes(self):
+        return api.c_server_restart_audio_routes()
     
     # ----------------------------
     # Change audio outputs for default routs (usually temporarily)    
@@ -75,13 +79,13 @@ cdef class Audio:
     
     # ----------------------------
     # Revert to configured routes  
-    def server_revert_audio_outputs():
+    def server_revert_audio_outputs(self):
         api.c_server_revert_audio_outputs()
     
     # ----------------------------
     # Run local audio services   
-    def server_local_audio_run(self runstate):
-        api.c_server_local_audio_run(runstate)
+    def server_local_audio_run(self, runstate):
+        return api.c_server_local_audio_run(runstate)
     
 # ===================================================
 # Radio functions
@@ -108,7 +112,86 @@ cdef class Radio:
     # Stop radio streaming  
     def radio_stop(self):
         return api.c_radio_stop()
- 
+    
+    #=============================
+    # Command functions
+    # ----------------------------
+    # Set frequency
+    def server_cc_out_set_rx_1_freq(self, freq_in_hz):
+        api.c_server_cc_out_set_rx_1_freq(freq_in_hz)
+    def server_cc_out_set_rx_2_freq(self, freq_in_hz):
+        api.c_server_cc_out_set_rx_2_freq(freq_in_hz)
+    def server_cc_out_set_rx_3_freq(self, freq_in_hz):
+        api.c_server_cc_out_set_rx_3_freq(freq_in_hz)
+    def server_cc_out_set_tx_freq(self, freq_in_hz):
+        api.c_server_cc_out_set_tx_freq(freq_in_hz)
+    
+    # ----------------------------
+    # Set duplex mode   
+    def server_cc_out_duplex(self, duplex):
+        api.c_server_cc_out_duplex(duplex)
+    
+    # ----------------------------
+    # Set HF preamp   
+    def server_cc_out_preamp(self, preamp):
+        api.c_server_cc_out_preamp(preamp)
+    
+    #=============================
+    # Alex filter functions
+    def server_cc_out_alex_attn(self, attn):
+        api.c_server_cc_out_alex_attn(attn)
+        
+    def server_cc_out_alex_rx_ant(self, ant):
+        api.c_server_cc_out_alex_rx_ant(ant)
+        
+    def server_cc_out_alex_rx_out(self, out):
+        api.c_server_cc_out_alex_rx_out(out)
+        
+    def server_cc_out_alex_tx_rly(self, rly):
+        api.c_server_cc_out_alex_tx_rly(rly)
+        
+    def server_cc_out_alex_auto(self, state):
+        api.c_server_cc_out_alex_auto(state)
+        
+    def server_cc_out_alex_hpf_bypass(self, bypass):
+        api.c_server_cc_out_alex_hpf_bypass(bypass)
+        
+    def server_cc_out_lpf_30_20(self, setting):
+        api.c_server_cc_out_lpf_30_20(setting)
+        
+    def server_cc_out_lpf_60_40(self, setting):
+        api.c_server_cc_out_lpf_60_40(setting)
+        
+    def server_cc_out_lpf_80(self, setting):
+        api.c_server_cc_out_lpf_80(setting)
+        
+    def server_cc_out_lpf_160(self, setting):
+        api.c_server_cc_out_lpf_160(setting)
+        
+    def server_cc_out_lpf_6(self, setting):
+        api.c_server_cc_out_lpf_6(setting)
+        
+    def server_cc_out_lpf_12_10(self, setting):
+        api.c_server_cc_out_lpf_12_10(setting)
+        
+    def server_cc_out_lpf_17_15(self, setting):
+        api.c_server_cc_out_lpf_17_15(setting)
+        
+    def server_cc_out_hpf_13(self, setting):
+        api.c_server_cc_out_hpf_13(setting)
+        
+    def server_cc_out_hpf_20(self, setting):
+        api.c_server_cc_out_hpf_20(setting)
+        
+    def server_cc_out_hpf_9_5(self, setting):
+        api.c_server_cc_out_hpf_9_5(setting)
+        
+    def server_cc_out_hpf_6_5(self, setting):
+        api.c_server_cc_out_hpf_6_5(setting)
+        
+    def server_cc_out_hpf_1_5(self, setting):
+        api.c_server_cc_out_hpf_1_5(setting)
+    
 # ===================================================
 # DSP functions
 cdef class DSP:
@@ -117,13 +200,18 @@ cdef class DSP:
     # Constructor
     def __cinit__(self):
         pass
-        
+    
+    # ----------------------------
+    # Make wisdom file
+    def server_make_wisdom(self, dir):
+        api.c_server_make_wisdom(dir)
+    
     #=============================
     # RX functions
     # ----------------------------
     # New RX mode
     def server_set_rx_mode(self, channel, mode):
-        api.c_server_set_rx_mode(int channel, int mode)
+        api.c_server_set_rx_mode(channel, mode)
         
     # ----------------------------
     # Filter run with settings   
@@ -153,7 +241,40 @@ cdef class DSP:
     # ----------------------------
     # Get last RX meter data for given type    
     def server_get_rx_meter_data(self, channel, which):
-        api.c_server_get_rx_meter_data(channel, which)
+        return api.c_server_get_rx_meter_data(channel, which)
+
+# ===================================================
+# Display functions
+cdef class Display:
+   
+    # ----------------------------
+    # Constructor
+    def __cinit__(self):
+        self.__display_data = np.zeros(1920, dtype=np.float)
+    
+    # ----------------------------
+    # Set display width
+    def server_set_display(self, ch_id, display_width):
+        api.c_server_set_display(ch_id, display_width)
+    
+    # ----------------------------
+    # Get last display data    
+    def server_get_display_data(self, display_id, display_data):
+        if api.c_server_get_display_data(display_id, np.PyArray_DATA(self.__display_data)):
+            return [True, self.__display_data]
+        else:
+            return [False, None]
+          
+    # ----------------------------
+    # Process WBS frame    
+    def server_process_wbs_frame(self, ptr_in_bytes):
+        api.c_server_process_wbs_frame(ptr_in_bytes)
+    
+    # ----------------------------
+    # Get WBS data    
+    def server_get_wbs_data(self, width, wbs_data):
+        # return api.c_server_get_wbs_data(width, wbs_data)
+        pass
     
 #=======================================================================================	
 # These are functions with C types. They can be called internally in Cython but cannot be exposed.
